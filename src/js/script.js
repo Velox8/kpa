e036a8c295dbeb580bad5451b906ce90f5b4bf14
Max <syst38@gmail.com>
2024-12-13 21:47:32 +0100

    first commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,21 @@
-const navMoblie = document.querySelector('.nav-mobile')
-const burgerBtn = document.querySelector('.burger')
-const navContainer = document.querySelector('.nav-container')
-
-
+const navMobile = document.querySelector('.nav-mobile');
+const burgerBtn = document.querySelector('.burger');
+const buttonText = document.querySelector('.button-container__p');
+const navContainer = document.querySelector('.nav-container');
 
 const handleNav = () => {
+	navMobile.classList.toggle('nav-mobile-active');
+	navContainer.classList.toggle('nav-container-active');
+	burgerBtn.classList.toggle('burger-active');
+};
 
-    navMoblie.classList.toggle('nav-mobile-active')
-    navContainer.classList.toggle(
-        'nav-container-active'
-    )
-    burgerBtn.classList.toggle('burger-active')
-}
-document.addEventListener("scroll", () => {
-    const navContainer = document.querySelector('.nav-container');
-    if (window.scrollY > 600) { // Moment, w którym nawigacja ma się przykleić
-        navContainer.classList.add('sticky');
-    } else {
-        navContainer.classList.remove('sticky');
-    }
+document.addEventListener('scroll', () => {
+	const navContainer = document.querySelector('.nav-container');
+	if (window.scrollY > 600) {
+		navContainer.classList.add('sticky');
+	} else {
+		navContainer.classList.remove('sticky');
+	}
 });
-
-
-
-
-burgerBtn.addEventListener('click',handleNav)
+burgerBtn.addEventListener('click', handleNav);
+buttonText.addEventListener('click', handleNav);
